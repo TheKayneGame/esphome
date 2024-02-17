f1323b7a422c8049df284f65a50dc092cf5008f1
Kayne <kayne.saridjo@gmail.com>
2024-02-17 01:17:27 +0100

    added ws2811

--- esphome/components/ct_ws2811/light.py
+++ esphome/components/ct_ws2811/light.py
@@ -2,8 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light, output
 from esphome.const import (
-    CONF_BRIGHTNESS,
-    CONF_COLOR_TEMPERATURE,
     CONF_OUTPUT_ID,
     CONF_COLD_WHITE_COLOR_TEMPERATURE,
     CONF_WARM_WHITE_COLOR_TEMPERATURE,
